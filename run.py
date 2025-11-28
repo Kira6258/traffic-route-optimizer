36dd5a95908a23bc38a65eac7dbaa987dfb97a9c
Kira <hhkiranesh@gmail.com>
2025-11-28 22:44:36 +0530

    latest

--- run.py
+++ run.py
@@ -5,8 +5,10 @@ from src.interface import create_app
 # Set up logging before starting the app
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
+    
     print("🚀 Starting Flask application...")
     
     app.run(debug=True)
